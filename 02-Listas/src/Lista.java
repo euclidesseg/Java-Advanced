eb199f3b3c829faf0c0239b42df12be1d6518544
euclidesseg <euclides2696@gmail.com>
2025-12-22 07:04:33 -0500

    se termina la seccion collections y se inicia con listas

---
+++ 02-Listas/src/Lista.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+public class Lista {
+    public static void main(String[] args){
+
+        /* Podemos declarar listas de la siguiente manera */
+        List linkedList = new LinkedList();
+        List arraryList = new ArrayList();
+        List vector = new Vector();
+
+    }
+}
---
+++ 02-Listas/src/Main.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        /* Casos comunes de usos de listas*/
+
+
+        // Convertir un array en una lista
+
+        String[] names = {"Jonas","Noah","Marta","Eva"};
+        System.out.println("Array: " + Arrays.toString(names));
+
+        List<String> arrayListNames = new ArrayList<String>();
+
+        for (String name : names){
+            arrayListNames.add(name);
+        }
+        System.out.println("List: " + arrayListNames);
+        System.out.println("==================");
+
+        // Convertir una Lista en un array
+        List<String> daysList =  new ArrayList<String>();
+        daysList.add("Lunes");
+        daysList.add("Martes");
+        daysList.add("Miércoles");
+        daysList.add("Jueves");
+        daysList.add("Viernes");
+        daysList.add("Sábado");
+        System.out.println("List: " +daysList);
+        String[] daysArray = daysList.toArray(new String[daysList.size()]);
+        System.out.println("Array: " +Arrays.toString(daysArray));
+
+        //Obtener y actualizar elementos de una lista
+        List<String> letters = new ArrayList<String>();
+        letters.add("A");
+        letters.add("B");
+        letters.add("C");
+
+        System.out.println("Elementos en el indice 1: "+ letters.get(1));
+
+        letters.set(2, "D");
+
+        for (String letter : letters){
+            System.out.println(letter);
+        }
+    }
+}
