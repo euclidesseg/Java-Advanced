aebf8ebcfe4e1d6fca46c60e4d0993250de34268
euclidesseg <euclides2696@gmail.com>
2026-02-18 21:51:13 -0500

    update documentation of linkedlist

--- 04-LinkedList/src/Main.java
+++ 04-LinkedList/src/Main.java
@@ -4,42 +4,43 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
 
-//        ArrayList<String> colores = new ArrayList<String>();
-//        colores.add("Rojo");
-//        colores.add("Azul");
-//        colores.add("Amarillo");
-//        colores.add("Naranja");
-//
-//        System.out.println("Colores" + colores);
-
         LinkedList<String> linkedList = new LinkedList<String>();
 
-        // las linkedList tienen comportamiento de pilas (LIFO)
+        // Las linkedList tienen comportamiento de pilas (LIFO) Último en entrar, primero en salir
+        // Para lograrlo, insertamos y eliminamos siempre por el mismo extremo (el inicio).
+        // push() agrega al inicio (addFirst)
+
 //        linkedList.push("A");
 //        linkedList.push("B");
 //        linkedList.push("C");
 //        linkedList.push("D");
-//        linkedList.push("F");
-//
-//        String removed =  linkedList.removeFirst(); // remueve a F ya que se comporta como pila
-//        linkedList.pop();// elimina a F
+//        linkedList.push("F"); // último en entrar
+        // si imprimimos esta lista mediante for el primero elemento que se imprimirá será F
 
-        // Las linkedList también pueden ser tratadas como Queue (FIFO)
+        //String removed = linkedList.removeFirst();
+        //linkedList.pop();
+        // removeFirst() o pop() eliminan del inicio por lo que en ambos se elimina a F
+
+
+
+        // Las linkedList también pueden ser tratadas como Queue (FIFO) Primero en entrar, primero en salir
+        // Para lograrlo, insertamos al final y eliminamos del inicio.
+        // offer() agrega al final (addLast)
         linkedList.offer("A");
         linkedList.offer("B");
         linkedList.offer("C");
         linkedList.offer("D");
         linkedList.offer("F");
 
-//        linkedList.poll();
+        linkedList.poll(); // elimina a "A"
 
 //        En los lugares donde las listas enlazadas tienden a tener ventajas sobre las listas y las listas de arreglos es
 //        en la inserción y eliminación de nodos
-        linkedList.add(4,"E");
-        linkedList.remove("E");
+//        linkedList.add(4,"E");
+//        linkedList.remove("E");
 
         /// Dado que LinkedList no está optimizada para el acceso por índice,
-        // sino para el recorrido secuencial, la forma correcta de recorrerla
+        // si no para el recorrido secuencial, la forma correcta de recorrerla
         // es mediante un for-each (Iterator).
 
         for (String linked: linkedList){
