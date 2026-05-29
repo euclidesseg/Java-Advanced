9f169d3638a588482b256cf0c658d15be187876f
euclidesseg <euclides2696@gmail.com>
2026-05-28 23:23:50 -0500

    update notes of linkedlist

--- 04-LinkedList/src/Main.java
+++ 04-LinkedList/src/Main.java
@@ -6,33 +6,36 @@ public class Main {
 
         LinkedList<String> linkedList = new LinkedList<String>();
 
-        // Las linkedList tienen comportamiento de pilas (LIFO) Último en entrar, primero en salir
-        // Para lograrlo, insertamos y eliminamos siempre por el mismo extremo (el inicio).
-        // push() agrega al inicio (addFirst)
+//         Las linkedList tienen comportamiento de pilas (LIFO) Último en entrar, primero en salir
+//         Para lograrlo, insertamos y eliminamos siempre por el mismo extremo (el inicio).
+//         push() agrega al inicio (addFirst)
 
 //        linkedList.push("A");
 //        linkedList.push("B");
 //        linkedList.push("C");
 //        linkedList.push("D");
 //        linkedList.push("F"); // último en entrar
-        // si imprimimos esta lista mediante for el primero elemento que se imprimirá será F
-
-        //String removed = linkedList.removeFirst();
-        //linkedList.pop();
-        // removeFirst() o pop() eliminan del inicio por lo que en ambos se elimina a F
+//        // si imprimimos esta lista mediante for el primero elemento que se imprimirá será F
+//
+//        String removed = linkedList.removeFirst();
+//        System.out.println(linkedList);
+//        linkedList.pop();
+        //removeFirst() o pop() eliminan del inicio por lo que estamos eliminando F
 
 
 
         // Las linkedList también pueden ser tratadas como Queue (FIFO) Primero en entrar, primero en salir
         // Para lograrlo, insertamos al final y eliminamos del inicio.
         // offer() agrega al final (addLast)
+
         linkedList.offer("A");
         linkedList.offer("B");
         linkedList.offer("C");
         linkedList.offer("D");
         linkedList.offer("F");
 
-        linkedList.poll(); // elimina a "A"
+        linkedList.poll();
+        // elimina a "A"
 
 //        En los lugares donde las listas enlazadas tienden a tener ventajas sobre las listas y las listas de arreglos es
 //        en la inserción y eliminación de nodos
